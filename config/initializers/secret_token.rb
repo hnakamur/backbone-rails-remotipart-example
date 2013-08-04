3494086ad8ab54701024b8e86b2d2000181a1d35
Hiroaki Nakamura <hnakamur@gmail.com>
2013-08-04 23:37:28 +0900

    Initial commit for a newly generated app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AjaxUploadSample::Application.config.secret_key_base = 'c79fe3ed415586e1adc672e795317ec7e70fa891b845106aa25dff9dd30507259d2d0c5ff743048de33cd3c70ef3bd2c8a6cd060dad85cc6405ed095fd0a736d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AjaxUploadSample::Application.config.session_store :cookie_store, key: '_ajax_upload_sample_session'
